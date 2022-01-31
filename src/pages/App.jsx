75e102610f5c0184bf500393b051a591be2673c1
Vitalii Horoshko <vitaliihoroshko@gmail.com>
2022-01-31 21:25:03 +0200

    Create ButtonAddTransactions component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import Layout from 'components/Layout';
 import Currency from 'components/Currency';
+import ButtonAddTransactions from 'components/Buttons/ButtonAddTransactions';
 
 const App = () => {
   return (
     <Layout>
       <Currency />
+      <ButtonAddTransactions />
     </Layout>
   );
 };
