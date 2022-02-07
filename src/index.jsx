c05abf8b324c6cc299d9a903c174afe7f63ffe7c
artemkarimov <tymwrk1@gmail.com>
2022-02-07 20:55:19 +0200

    Add redux packages, create redux store and provide it to the app

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './pages/App';
+import store from 'store';
 import './styles/index.scss';
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
   document.getElementById('root'),
