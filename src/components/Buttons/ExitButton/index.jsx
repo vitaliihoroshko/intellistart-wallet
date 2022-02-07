3ab279e24368590b8ef5538b24a2cf2214e2e558
artemkarimov <tymwrk1@gmail.com>
2022-02-07 22:09:54 +0200

    Add store state and handlers that dispatch store actions to Header and ExitButton components

--- src/components/Buttons/ExitButton/index.jsx
+++ src/components/Buttons/ExitButton/index.jsx
@@ -1,9 +1,25 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { signUserOut } from 'api/api-helper';
+import { authSliceActions } from 'store/slices/authSlice';
 import exitIcon from 'assets/images/exit-icon.svg';
 import styles from './styles.module.scss';
 
 const ExitButton = () => {
+  const token = useSelector(state => state.auth.token);
+  const dispatch = useDispatch();
+
+  const clickHandler = async () => {
+    try {
+      await signUserOut(token);
+      dispatch(authSliceActions.signUserOut());
+    } catch (error) {
+      dispatch(authSliceActions.setError('Bearer auth failed'));
+    }
+  };
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={clickHandler}>
       <img src={exitIcon} alt="exit-icon" width={18} height={18} className={styles['exit-icon']} />
       <span className={styles.exit}>Exit</span>
       &nbsp;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,19 @@
+import { useSelector } from 'react-redux';
+
 import Logo from 'components/Logo';
 import ExitButton from 'components/Buttons/ExitButton';
 import divider from 'assets/images/divider.svg';
 import styles from './styles.module.scss';
 
 const Header = () => {
+  const user = useSelector(state => state.auth.user);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Logo />
       </div>
-      <p className={styles['user-name']}>Name</p>
+      <p className={styles['user-name']}>{user ? user.username : 'Name'}</p>
       <img src={divider} alt="divider" width={2} height={30} className={styles.divider} />
       <ExitButton />
     </header>
