b35a5874bdefd2daaaf245027842adfaeab40ce8
artemkarimov <tymwrk1@gmail.com>
2022-02-07 21:12:43 +0200

    Create redux toolkit auth slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import authSliceReducer from './slices/authSlice';
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSliceReducer,
+  },
 });
 
 export default store;
