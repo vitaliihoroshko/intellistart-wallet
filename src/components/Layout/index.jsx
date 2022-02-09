1d30766c50af2b2684cd73c923a42cc24865b944
Vitalii Horoshko <vitaliihoroshko@gmail.com>
2022-02-09 12:49:36 +0200

    Fix AddTransactionButton component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,11 @@
 import Header from 'components/Header';
 import PropTypes from 'prop-types';
+import AddTransactionsButton from 'components/Buttons/AddTransactionsButton';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <AddTransactionsButton />
       <Header />
       <main>{children}</main>
     </>
