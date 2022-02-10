805bab6920e0ab7a442d0463b5e735009392a408
artemkarimov <tymwrk1@gmail.com>
2022-02-10 23:17:44 +0200

    Add api argument to redux thunks

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import sessionReducer from './slices/session';
+import * as api from 'api/api-helper';
 
 const store = configureStore({
   reducer: {
     session: sessionReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ thunk: { extraArgument: api } }),
 });
 
 export default store;
