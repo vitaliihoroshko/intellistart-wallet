b5ca707ffd940e5918e35c79088c4626e14a02c8
artemkarimov <tymwrk1@gmail.com>
2022-02-14 17:07:42 +0200

    Create redux toolkit global slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import sessionReducer from './slices/session';
+import globalReducer from './slices/global';
 import * as api from 'api/api-helper';
 
 const store = configureStore({
   reducer: {
     session: sessionReducer,
+    global: globalReducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware({ thunk: { extraArgument: api } }),
 });
