83b8200440a4ad2ce9b6d6706f0451e9686e90c0
Ostap Blystsiv <ostapcr7@gmail.com>
2022-02-15 19:01:42 +0200

    correct styles

--- src/components/StatisticTable/index.jsx
+++ src/components/StatisticTable/index.jsx
@@ -70,6 +70,18 @@ const StatisticTable = () => {
     getCellProps = defaultPropGetter,
   } = tableInstance;
 
+  // const categoryColor = [
+  //   '#FED057',
+  //   '#FFD8D0',
+  //   '#FD9498',
+  //   '#C5BAFF',
+  //   '#6E78E8',
+  //   '#4A56E2',
+  //   '#81E1FF',
+  //   '#24CCA7',
+  //   '#00AD84',
+  // ];
+
   function StatTable() {
     return (
       <div className={styles['dashboard']}>
