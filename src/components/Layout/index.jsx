bd6ea48f76ba9079328e278631153c5b6b82031b
Vitalii Horoshko <vitaliihoroshko@gmail.com>
2022-02-16 02:44:52 +0200

    Add ModalAddTransaction and AddTransactionsButton on Dashboard page

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,16 +1,12 @@
 import { node } from 'prop-types';
 
 import Header from 'components/Header';
-import AddTransactionsButton from 'components/Buttons/AddTransactionsButton';
-import ModalAddTransaction from 'components/Modals/ModalAddTransaction';
 
 const Layout = ({ children }) => {
   return (
     <>
-      <ModalAddTransaction />
       <Header />
       <main>{children}</main>
-      <AddTransactionsButton />
     </>
   );
 };
