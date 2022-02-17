74d6d5bfdcf4d820be16de0bc3a4614fe768e0b9
Alina Prokopenko <alina.chukevich98@gmail.com>
2022-02-17 17:05:20 +0200

    Home page styles

--- src/components/Balance/index.jsx
+++ src/components/Balance/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { useSelector } from 'react-redux';
-import { getCurrentUser } from 'api/api-helper';
+// import { useSelector } from 'react-redux';
+// import { getCurrentUser } from 'api/api-helper';
 const Balance = () => {
-  let userData;
-  async () => {
-    userData = await getCurrentUser();
-  };
-  console.log(userData);
+  // let userData;
+  // async () => {
+  // userData = await getCurrentUser();
+  // };
+  // console.log(userData);
   return (
     <div className={styles['balance']}>
       <p className={styles['balance__text']}>Your balance</p>
