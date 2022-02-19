38fdd8cbdcaccd785b83835a75c753477194edae
artemkarimov <tymwrk1@gmail.com>
2022-02-20 00:37:48 +0200

    Add Link to Not Found page and create route for Not Found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,17 +1,24 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import styles from './styles.module.scss';
 
-const NotFound = () => {
+const NotFoundPage = () => {
+  const { isAuth } = useSelector(state => state.session);
+
   return (
     <>
       <div className={styles.background}>
         <div className={styles.wrapper}>
           <h1 className={styles.error}>404 error</h1>
           <p className={styles.msg}>Oh, you seem to be lost ...</p>
-          <button className={styles.link}>home</button>
+          <Link to={isAuth ? '/home' : '/login'} replace className={styles.link}>
+            {isAuth ? 'home' : 'login'}
+          </Link>
         </div>
       </div>
     </>
   );
 };
 
-export default NotFound;
+export default NotFoundPage;
